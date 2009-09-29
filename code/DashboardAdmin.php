01ac2119988dc24fb07ba24422cf01e53b30f4e4
Morven Lewis-Everley <morven@i-lateral.com>
2009-09-29 22:58:59 +0000

    Add SS RSS feed to left hand panel

--- code/DashboardAdmin.php
+++ code/DashboardAdmin.php
@@ -1,12 +1,13 @@
 <?php
- 
+ /**
+  * Main dashboard class. Auto creates a link in the CMS by being extended from LeftAndMain.
+  * 
+  * @package Dashboard
+  */
 class DashboardAdmin extends LeftAndMain {
 	static $url_segment = '';
-	
 	static $menu_title = 'Dashboard';
-	
 	static $menu_priority = 99;
-	
 	static $url_priority = 41;
 
 	/**
@@ -19,24 +20,47 @@ class DashboardAdmin extends LeftAndMain {
 		Requirements::customScript(file_get_contents(Director::baseFolder() . '/dashboard/javascript/Dashboard.js'));
 	}
 	
+	/**
+	 * SiteInfo gets a wuick summary of all items in SiteTree, all Files (excluding folders) and members.
+	 * 
+	 * @return DataObjectSet
+	 */
 	public function SiteInfo() {
+		$output = new DataObjectSet();
+		
 		$pages		= DataObject::get('SiteTree');
 		$pages		= $pages ? $pages->Count() : 0;
 		$pagesStr	= ($pages == 1) ? 'page' : 'pages';
+		$output->push(new ArrayData(array(
+			'Number' => $pages,
+			'Item' => $pagesStr
+		)));
 
 		$files		= DataObject::get('File', "ClassName <> 'Folder'");
 		$files		= $files ? $files->Count() : 0;
 		$filesStr	= ($files == 1) ? 'file' : 'files';
+		$output->push(new ArrayData(array(
+			'Number' => $files,
+			'Item' => $filesStr
+		)));
 		
 		$members	= DataObject::get('Member');
 		$members	= $members ? $members->Count() : 0;
 		$membersStr	= ($members == 1) ? 'member' : 'members';
-
-		$output = "$pages $pagesStr<br/>$files $filesStr<br/>$members $membersStr";
+		$output->push(new ArrayData(array(
+			'Number' => $members,
+			'Item' => $membersStr
+		)));
 		
 		return $output;
 	}
 	
+	/**
+	 * RecentPages, RecentFiles, CommentUMod all get a list of dataobjects, either 10 most recent pages, 10 most recent files
+	 * or 10 most recent unmoderated comments.
+	 * 
+	 * @return DataObjectSet 
+	 */
 	public function RecentPages() {
 		$pages = DataObject::get("SiteTree", NULL, "`LastEdited` DESC", NULL, "0, 10");
 		return $pages;
@@ -50,6 +74,46 @@ class DashboardAdmin extends LeftAndMain {
 	public function CommentUMod() {
 		$pages = DataObject::get("PageComment", "NeedsModeration = '1'", "`Created` DESC", NULL, "0, 10");
 		return $pages;
+	}
+	
+	/**
+	 * Uses SimplePie to pull in the Silverstripe news feed, process and cast it, then output it to a DataObjectSet
+	 * 
+	 * @return DataObjectSet 
+	 */
+	
+	function LatestSSNews() {
+		$output = new DataObjectSet();
+		
+		include_once(Director::getAbsFile(SAPPHIRE_DIR . '/thirdparty/simplepie/SimplePie.php'));
+		
+		$t1 = microtime(true);
+		$feed = new SimplePie('http://www.silverstripe.org/blog/rss', TEMP_FOLDER);
+		$feed->init();
+		if($items = $feed->get_items(0, 2)) {
+			foreach($items as $item) {
+				
+				// Cast the Date
+				$date = new Date('Date');
+				$date->setValue($item->get_date());
+
+				// Cast the Title
+				$title = new Text('Title');
+				$title->setValue($item->get_title());
+				
+				// Cast the description and strip
+				$desc = new Text('Description');
+				$desc->setValue(strip_tags($item->get_description()));
+
+				$output->push(new ArrayData(array(
+					'Title'			=> $title,
+					'Date'			=> $date,
+					'Link'			=> $item->get_link(),
+					'Description'	=> $desc
+				)));
+			}
+			return $output;
+		}
 	}
 }
 
--- templates/DashboardAdmin_left.ss
+++ templates/DashboardAdmin_left.ss
@@ -1,7 +1,20 @@
 <div class="Dashboard">
 	<div class="dashboardItem SiteInfo">
 		<h2>Your site</h2>
-		<p>At a glance:<br/><br/>
-		$SiteInfo</p>
+		<p>At a glance:</p>
+		<% if SiteInfo %><ul>
+			<% control SiteInfo %><li>$Number $Item</li><% end_control %>
+		</ul><% end_if %>
+	</div>
+	<div class="dashboardItem SSNews">
+		<h2>Silverstripe news</h2>
+		<ul>
+			<% control LatestSSNews %><li>
+				<strong>$Title</strong><br/>
+				$Description.Summary(20)...<br/>
+				<a href='$Link' target="_blank">Read More</a>
+			</li><% end_control %>
+			<li><a href="http://www.silverstripe.org/blog/" target="_blank">View more stories</a></li>
+		</ul>
 	</div>
 </div>
