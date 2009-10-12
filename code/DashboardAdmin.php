aa9d4b98a7cd18fa3e24994b0644a3f4bfc9949a
Morven Lewis-Everley <morven@i-lateral.com>
2009-10-12 11:37:46 +0000

    Replace DOM XML with Regex to find latest SS version

--- code/DashboardAdmin.php
+++ code/DashboardAdmin.php
@@ -153,34 +153,32 @@ class DashboardAdmin extends LeftAndMain {
 	
 	public function GetUpdates() {
 		// Initial variables about SVN location and Current version
-		$url = 'http://svn.silverstripe.com/open/modules/sapphire/tags/';
-		$verList = array();
+		$verList = $aItems = array();
 		$curVersion = LeftAndMain::CMSVersion();
 		$curVersion = floor(str_replace(array('.','/'),'',$curVersion));
-		
+
 		// Get HTML from Silverstripe SVN browser, then pull out all A tags
-		$doc = new DOMDocument();
-		$doc->loadHTMLFile($url);
-		$items = $doc->getElementsByTagName('a');
+		$sRequest = HTTP::sendRequest('svn.silverstripe.com', '/open/modules/sapphire/tags/', null);
+		preg_match_all('#<a[^<]*>([^<]*)</a>#i', $sRequest, $aItems, PREG_SET_ORDER);
 		
 		// Loop trough all A tags and attempt to convert their value to Int. If success
-		// add the value to $verList array. 
-		foreach($items as $item) {
-			$result = $item->nodeValue;
+		// add the value to $verList array.
+		foreach($aItems as $aItem) {
+			$result = $aItem[1];
 			$strip = str_replace(array('.','/'), '', $result);
 			
 			if(floor($strip))
 				array_push($verList,$result);
 		}
-		
-		// Retrieve the last item in the $verList array and converty to int
-		$latest = floor(str_replace(array('.','/'), '', $verList[count($verList) - 1]));
-		
-		// If latest version if later than current version, return an update message.
-		if($latest > $curVersion)
-			return 'Silverstripe ' . str_replace('/','',$verList[count($verList) - 1]) . ' is available. The latest version can be found <a href="' . self::$ss_link . '" title="Silverstripe download page">here</a>, or update your SVN.';
-		
-	}
+
+      // Retrieve the last item in the $verList array and converty to int
+      $latest = floor(str_replace(array('.','/'), '', $verList[count($verList) - 1]));
+
+      // If latest version if later than current version, return an update message.
+      if($latest > $curVersion)
+         return 'Silverstripe ' . str_replace('/','',$verList[count($verList) - 1]) . ' is available. The latest version can be found <a href="' . self::$ss_link . '" title="Silverstripe download page">here</a>, or update your SVN.';
+
+   }
 }
 
 ?>
