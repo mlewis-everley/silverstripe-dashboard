113e1e8c35c2b78c6c1de64822c2bb968349cea8
Morven Lewis-Everley <morven@i-lateral.com>
2009-10-12 22:01:01 +0000

    Remove old reduntant translation code

--- lang/en_US.php
+++ lang/en_US.php
@@ -2,16 +2,5 @@
 
 global $lang;
 
-$lang['en_US']['Dashboard.ss']['TODOLIST'] = 'Your To-do List';
-$lang['en_US']['Dashboard.ss']['NOITEMS'] = 'No Items';
-$lang['en_US']['Dashboard.ss']['SITEINFORMATION'] = 'Site Information';
-$lang['en_US']['DashboardWidget_Area.ss']['EDITWIDGET'] = 'Edit Widget Settings';
-$lang['en_US']['DashboardWidget_Area.ss']['ABOUTWIDGET'] = 'About This Widget';
-$lang['en_US']['DashboardWidget_Area.ss']['REMOVEWIDGET'] = 'Remove Widget';
-$lang['en_US']['DashboardWidget_about.ss']['ABOUTWIDGET'] = 'About The %s Widget';
-$lang['en_US']['DashboardWidget_about.ss']['DESCRIPTION'] = 'Description';
-$lang['en_US']['DashboardWidget_add.ss']['SELECTWIDGET'] = 'Select a Widget';
-$lang['en_US']['DashboardWidget_configure.ss']['CONFIGUREWIDGET'] = 'Configure Widget';
-$lang['en_US']['DashboardWidget_edit.ss']['CONFIGUREWIDGET'] = 'Configure Widget';
 
 ?>
