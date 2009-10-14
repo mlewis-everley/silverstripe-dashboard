94d76051cdf0391e66c4540e5cfc8aeb46b03582
Morven Lewis-Everley <morven@i-lateral.com>
2009-10-14 21:23:59 +0000

    Rewrite to use generic variables for template

--- code/dashboard_plugins/RecentPages.php
+++ code/dashboard_plugins/RecentPages.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Snippets have the following generic variables, that can be used in generic templates:
+ * 
+ * $Title
+ * $URL
+ * $Date
+ * $ID
+ */
+
 
 class RecentPages extends DashboardPlugin {
 	static $position = "snippet";
@@ -9,9 +18,25 @@ class RecentPages extends DashboardPlugin {
 	static $caption = "Your site's most recently edited pages.";
 	static $null_message = 'You site currently has no pages';
 	static $edit_link = 'admin/cms/show';
-
+	
 	public function GenericSnippetList() {
-		$result = DataObject::get("SiteTree", NULL, "`LastEdited` DESC", NULL, "0,".self::$limit_count);
-		return $result;
+		$output = new DataObjectSet();
+		
+		$items = DataObject::get("SiteTree", NULL, "`LastEdited` DESC", NULL, "0,".self::$limit_count);
+		
+		if($items) {
+			foreach($items as $item) {
+				$date = new Date('Date');
+				$date->setValue($item->LastEdited);
+				
+				$output->push(new ArrayData(array(
+					'URL'		=> Director::baseURL().$item->URLSegment,
+					'Title'		=> $item->Title,
+					'Date'		=> $date,
+					'ID'		=> $item->ID
+				)));
+			}
+		}
+		return $output;
 	}
 }
