62a7746c51d15d8abe95b862357a0a1323beabd6
Morven Lewis-Everley <morven@i-lateral.com>
2009-10-24 00:01:55 +0000

    Add edit page and cms links as sperate, CMS goes to dashboard, Edit page goes to page in CMS

--- code/SSNController.php
+++ code/SSNController.php
@@ -1,6 +1,20 @@
 <?php
+/**
+ * SSNController adds a new method to Content Controller, this replaces the default Silverstripe
+ * Navigator. Instead of hard codede links, SSNavigator should allow URL's in the navigator to
+ * be altered a bit more easily and should allow for custom templating within the users
+ * Templates folder.
+ * 
+ * To add The new navigator bar, add $SSNavigator to your template, in place of the traditional
+ * $Silverstripenavigator
+ */
 class SSNController extends Extension {
 	protected $dataRecord;
+	
+	/**
+	 * @var $adminLink Link to the main admin area 
+	 */
+	static $adminLink = 'admin';
 
 	public function __construct($dataRecord = null) {
 		parent::__construct();
@@ -30,7 +44,8 @@ class SSNController extends Extension {
 JS
 			);
 			
-			$this->owner->cmsLink = "admin/".CMSMain::$url_segment."/show";
+			$this->owner->cmsLink	= self::$adminLink."/".CMSMain::$url_segment."/show";
+			$this->owner->adminLink	= self::$adminLink;
 			
 			if($date = Versioned::current_archived_date()) {
 				$this->owner->DisplayMode ='Archived'; 
