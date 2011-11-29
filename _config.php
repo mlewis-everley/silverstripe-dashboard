0c90b90e1468ed898399a1d4a24641b2ecf07fa7
morven <morven@oomphagency.com>
2011-11-29 16:17:05 +0000

    Add check to ensure  is in use (stopping the CLI from breaking)

--- _config.php
+++ _config.php
@@ -1,16 +1,19 @@
 <?php
-$curUrl = explode("?",$_SERVER['REQUEST_URI']);
 
-if(
-    ($curUrl[0] == Director::baseURL() . 'admin/' ||
-    $curUrl[0] == Director::baseURL() . 'admin') &&
-    (substr_count(strtolower($curUrl[1]), 'locale') == 0)
-)
-{
-    Director::addRules(50, array(
-        'admin' => '->admin/dashboard/'
-    ));
+// Test is $_SERVER['REQUEST_URI'] is available
+if (isset($_SERVER['REQUEST_URI'])) {
+    $curUrl = explode("?",$_SERVER['REQUEST_URI']);
+
+    if(
+        ($curUrl[0] == Director::baseURL() . 'admin/' ||
+        $curUrl[0] == Director::baseURL() . 'admin') &&
+        (substr_count(strtolower($curUrl[1]), 'locale') == 0)
+    )
+    {
+        Director::addRules(50, array(
+            'admin' => '->admin/dashboard/'
+        ));
+    }
 }
-
 CMSMain::$url_segment = 'cms';
 CMSMenu::replace_menu_item('CMSMain', 'Site Content', 'admin/cms/', 'CMSMain', 41);
